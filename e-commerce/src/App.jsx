bb31460c3ad5bfa9f201894d8ea40a2b5485445c
MAX1NITY <mariobomausanga@gmail.com>
2024-12-07 10:30:45 -0800

    save 1

---
+++ e-commerce/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Categories, Footer, Header, Navbar, Products} from "./components"
+
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Products />
+    <Categories />
+    <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ e-commerce/src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./Header/Header";
+import Categories from "./Categories/Categories";
+import Footer from "./Footer/Footer";
+import Navbar from "./Navbar/Navbar";
+import Products from "./Products/Products";
+
+export{
+    Categories,
+    Footer,
+    Header,
+    Navbar,
+    Products
+}
+
+
+
+
